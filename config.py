385788cac38f7869ea515db6315d1e1f0fa6cf86
Litingjun <739142578@qq.com>
2016-06-06 11:28:57 +0800

    sae mysql

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 # -*-coding:utf-8 -*-
+import sae.const
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 
 
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://ACCESSKEY:SECRETKEY@MYSQL_HOST/app_yzxtest'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://'+sae.const.MYSQL_USER+':'+sae.const.MYSQL_PASS+'@'+sae.const.MYSQL_HOST+'/'+sae.const.MYSQL_DB
 SQLALCHEMY_TRACK_MODIFICATIONS=True
 
 MAIL_SERVER='smtp.qq.com'
@@ -15,7 +16,7 @@ MAIL_PASSWORD=''
 MAIL_USE_TLS = False
 ADMINS=['']
 
-WHOOSH_BASE='mysql+pymysql://'+ACCESSKEY+':'+SECRETKEY+'@'+MYSQL_HOST+'/app_yzxtest'
+WHOOSH_BASE='mysql+pymysql://'+sae.const.MYSQL_USER+':'+sae.const.MYSQL_PASS+'@'+sae.const.MYSQL_HOST+'/'+sae.const.MYSQL_DB
 MAX_SEARCH_RESULTS = 50
 
 
