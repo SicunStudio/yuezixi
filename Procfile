1b3697da4e9380c4b77a2a377e3a29378bd30ed9
Litingjun <739142578@qq.com>
2016-06-06 21:40:50 +0800

    fix

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python run1.py
--- config.py
+++ config.py
@@ -2,18 +2,18 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://z33mo3z054:y2ikxw40i21xl2iji3h3yjwmxxxk0lyjlh510i1k@w.rdc.sae.sina.com.cn:3307/app_yzxtest'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost:3306/yzxtest'
 SQLALCHEMY_TRACK_MODIFICATIONS=True
 
-MAIL_SERVER='smtp.qq.com'
+MAIL_SERVER='smtp.163.com'
 MAIL_PORT=465
 MAIL_USE_SSL=True
-MAIL_USERNAME=''
-MAIL_PASSWORD=''
+MAIL_USERNAME='litingjun96@163.com'
+MAIL_PASSWORD='ltj0410ascop'
 MAIL_USE_TLS = False
-ADMINS=['']
+ADMINS=['litingjun96@163.com']
 
-WHOOSH_BASE='mysql+pymysql://z33mo3z054:y2ikxw40i21xl2iji3h3yjwmxxxk0lyjlh510i1k@w.rdc.sae.sina.com.cn:3307/app_yzxtest'
+WHOOSH_BASE='mysql+pymysql://root:@localhost:3307/yzxtest'
 MAX_SEARCH_RESULTS = 50
 
 #MAIL_DEFAULT_SENDER='系统'
