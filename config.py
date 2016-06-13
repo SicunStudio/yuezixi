fb2315c89081de5a20f6506382797179ebbbd004
Litingjun <739142578@qq.com>
2016-06-13 20:11:12 +0800

    update

--- config.py
+++ config.py
@@ -2,18 +2,27 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost:3306/yzxtest'
+
+
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost/yzxtest'#mysql的配置
 SQLALCHEMY_TRACK_MODIFICATIONS=True
 
-MAIL_SERVER='smtp.163.com'
+MAIL_SERVER=''#邮箱服务器
 MAIL_PORT=465
 MAIL_USE_SSL=True
-MAIL_USERNAME='litingjun96@163.com'
-MAIL_PASSWORD='ltj0410ascop'
+MAIL_USERNAME=''#如果是qq邮箱则为qq号，136邮箱同理
+MAIL_PASSWORD=''#客户端密码
 MAIL_USE_TLS = False
-ADMINS=['litingjun96@163.com']
+MAIL=('','')#发件人如('me','1412511544@qq.com')
+ADMINS=['']#接收错误的邮箱的邮箱地址
 
-WHOOSH_BASE='mysql+pymysql://root:@localhost:3307/yzxtest'
+
+WHOOSH_BASE='mysql+pymysql://root:@localhost/yzxtest'
 MAX_SEARCH_RESULTS = 50
+
+
+
+
+
 
 #MAIL_DEFAULT_SENDER='系统'
